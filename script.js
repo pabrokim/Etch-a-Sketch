c393e845b79085e1acfb234e66b49da39265fac0
Paul Kimani <pabrokimani@gmail.com>
2023-04-04 20:45:33 +0300

    Dynamically generate grids

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
+const head = document.createElement('h1');
+head.classList.add('head');
+head.textContent='Etch-A-Sketch';
+document.body.appendChild(head);
+
+const button = document.createElement('button');
+button.classList.add('button');
+button.textContent='Set Number of Boxes';
+document.body.appendChild(button);
+
 const div = document.createElement('div');
 div.classList.add('container');
 document.body.appendChild(div);
 
-const container = document.querySelector('.container');
-
-function addDiv() {
-
-    for(let i = 1; i <=256; i++){ 
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        container.appendChild(grid);
-    }
-}
-
-addDiv();
-
-const items = document.querySelectorAll('.grid');
-
-items.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-    item.style.backgroundColor = 'green';
-    })
+button.addEventListener('click', () => {
+        let x = parseInt(prompt('Choose boxes per side'));
+        if(x < 1 || x > 100){
+            alert('Please enter a number between 1 and 100')
+        }
+        clearGrid();
+        createGrids(x);
 });
 
 
+const container = document.querySelector('.container');
+let containerWidth = container.offsetWidth;
+let containerHeight = container.offsetHeight;
 
+//creates grids
+function createGrids(x) {
+    for(let row = 0; row < x; row++){ 
+        for(let column = 0; column < x; column++){
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            grid.style.width = (containerWidth/x) +'px';
+            grid.style.height = (containerHeight/x) +'px';
+            container.appendChild(grid);
+        }
+    }
+}
+createGrids(16);
+
+//clear grids
+
+function clearGrid() {
+    let parent = document.querySelector('.container');
+    let child = parent.querySelectorAll('.grid');
+      for (let i = 0; i < child.length; i++) {
+        parent.removeChild(child[i]);
+
+    }
+}
+
+//mouse hover effect for dynamically created grids
+
+const gridContainer = document.querySelector('.container');
+gridContainer.addEventListener('mouseover', (e) => {
+    if(e.target.classList.contains('grid')){
+        e.target.style.backgroundColor = 'black';
+    }
+})
